da4e31709dba654f7c5e0a5f7e6a442687e3b894
Arseniy <joab.k1b3rnet1k@protonmail.com>
2021-11-10 15:52:04 -0100

    updated

---
+++ 1/1/cmake-build-debug/CMakeFiles/1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1"
+  "1.pdb"
+  "CMakeFiles/1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
